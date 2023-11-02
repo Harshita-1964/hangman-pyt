d302a339c56608cede4c105721b6d40b997a3fa6
Harshita Deepthimahanty <102470981+Harshita-1964@users.noreply.github.com>
2023-11-02 11:04:10 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,7 @@
+import random
+from hangman_words import word_list
+from hangman_graphics import HANGMAN_IMAGES
+from hangman_functions import get_random_word, display_board, get_guess, play_hangman
+
+if __name__ == "__main__":
+    play_hangman()
