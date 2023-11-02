261cf1d2ef0d49037085c091079d5def341f723a
Harshita Deepthimahanty <102470981+Harshita-1964@users.noreply.github.com>
2023-11-02 11:06:40 +0530

    Create hangman_functions.py

---
+++ hangman_functions.py
@@ -0,0 +1,11 @@
+def get_random_word(wordlist):
+    return random.choice(wordlist)
+
+def display_board(missed_letters, correct_letters, secret_word):
+    # function code
+
+def get_guess(already_guessed):
+    # function code
+
+def play_hangman():
+    # function code
